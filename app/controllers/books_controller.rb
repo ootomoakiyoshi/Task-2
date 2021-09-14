6173eed01b83d19db1724764c961ce9e123e9593
ootomoakiyoshi <ec2-user@ip-172-31-13-106.ec2.internal>
2021-09-14 13:31:52 +0000

    [add]Task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,14 +2,20 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
 
   def create
     @book = Book.new(book_params)
-    @book.save
+    if @book.save
     redirect_to book_path(@book.id)
+  elde
+    render :new
   end
+end
+
+
 
   def show
     @book = Book.find(params[:id])
@@ -19,9 +25,21 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+
+  end
+
 private
 def book_params
   params.require(:book).permit(:title, :body)
 end
-
 end
